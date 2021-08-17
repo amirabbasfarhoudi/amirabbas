bdbf3c1a4f74da10b3139b21914a5f7f49f7e9e6
amirabbasfarhoudi <89082549+amirabbasfarhoudi@users.noreply.github.com>
2021-08-17 17:53:12 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = prompt ("name :");
+var family = prompt ("family :");
+var password = prompt ("password :");
+var Repeatpassword = prompt ("Repeat password :")
+var gmail = prompt ("gmail :")
+
+
