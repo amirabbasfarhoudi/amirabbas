794611f7a7dd3446a904f62dc7301163d7831cff
amirabbasfarhoudi <89082549+amirabbasfarhoudi@users.noreply.github.com>
2021-08-17 18:06:03 +0430

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var name = prompt ("name :");
-var family = prompt ("family :");
-var password = prompt ("password :");
-var Repeatpassword = prompt ("Repeat password :")
-var gmail = prompt ("gmail :")
-
-
